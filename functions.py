c1a46d8ce43f32615120dd2248909cb1835fc51f
ReltonTolpo <ori.taylor@gmail.com>
2017-10-14 19:40:10 +0100

    Adding more functions
    Also changed first function

--- functions.py
+++ functions.py
@@ -1,7 +1,37 @@
-def userInput(text, answers):
+def userInput(content, answers, printNums):
 	while(True):
-		condition = input(text+" ("+((("{}")+"/")*len(answers)).format(*answers)[:-1]+") ").lower()
-		if(condition not in answers):
+		if(printNums == True):
+			output = input(content+" ("+("{}/"*len(answers)).format(*answers)[:-1]+") ").lower()
+		else:
+			output = input(content+" ").lower()
+		if(output not in answers):
 			print("Please input a valid answer")
 		else:
-			return(condition)
+			return(output)
+
+def inputInt(content):
+	while(True):
+		output = input(content)
+		try:
+			int(output)
+			return int(output)
+		except ValueError:
+			print("Please input an integer")
+
+def inputString(content):
+	while(True):
+		output = input(content)
+		try:
+			str(output)
+			return output
+		except ValueError:
+			print("Please input a string")
+
+def menu(options):
+	print("You have {} options:".format(len(options)))
+	numbers = []
+	for i in range(0,len(options)):
+		numbers.insert(i,str(i+1))
+		print("\n{}. {}".format(i+1,options[i]))
+	menuOption = int(userInput("\nInput one of the previous numbers:",numbers,False))
+	return menuOption
